04edcc353b5246cbceb86cd9bab2cb420e1e8efc
vishal <k.vishalkumbhar@gmail.com>
2019-11-27 18:08:11 +0530

    commit for abc

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", (req, res)=>{
+    res.send("welcome to git");
+})
+
+
+app.post("/", (req, res)=>{
+    res.send("welcome to git");
+})
+
+
+app.listen(7879, ()=>{
+    console.log("Srever Started at 7879...");
+})
---
+++ server2.js
@@ -0,0 +1,21 @@
+console.log("Server Started1..");
+
+
+console.log("Server Started.1.");
+
+
+
+
+console.log("Server Started..11");
+console.log("Server Started..");console.log("Server Started..");
+
+console.log("Server Started..");
+console.log("Server Started..");
+console.log("Server Started..");
+
+console.log("Server Started..");
+
+
+console.log("Server Started..");
+
+console.log("Server Started..");
